b0784527af7c7e6cc312eeab289e61922ea50fa2
Alex1512 <hellomozilla@gmail.com>
2016-04-12 21:41:50 +0800

    增加server入口

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-require('babel-polyfill');
-require('babel-core/register');
+//   神兽护体
+//   ┏┓       ┏┓
+//  ┏┛┻━━━━━━━┛┻┓
+//  ┃           ┃
+//  ┃     ━     ┃
+//  ┃  ┳┛   ┗┳  ┃
+//  ┃           ┃
+//  ┃```  ┻  ```┃
+//  ┃           ┃
+//  ┗━┓      ┏━━┛
+//    ┃      ┃   Code is far away from bug with the animal protecting.
+//    ┃      ┃   神兽护佑,代码无bug.
+//    ┃      ┗━━━┓
+//    ┃          ┣┓
+//    ┃          ┏┛
+//    ┗┓┓┏━━┳┓┏━━┛
+//     ┃┫┫  ┃┫┫
+//     ┗┻┛  ┗┻┛
+// require('babel-polyfill');
+require('babel-core/register')(
+  {
+    presets: ['es2015-node5', 'stage-3']
+  }
+);
 require('./server');
---
+++ server/index.js
@@ -0,0 +1,23 @@
+//   神兽护体
+//   ┏┓       ┏┓
+//  ┏┛┻━━━━━━━┛┻┓
+//  ┃           ┃
+//  ┃     ━     ┃
+//  ┃  ┳┛   ┗┳  ┃
+//  ┃           ┃
+//  ┃```  ┻  ```┃
+//  ┃           ┃
+//  ┗━┓      ┏━━┛
+//    ┃      ┃   Code is far away from bug with the animal protecting.
+//    ┃      ┃   神兽护佑,代码无bug.
+//    ┃      ┗━━━┓
+//    ┃          ┣┓
+//    ┃          ┏┛
+//    ┗┓┓┏━━┳┓┏━━┛
+//     ┃┫┫  ┃┫┫
+//     ┗┻┛  ┗┻┛
+
+import Koa from 'koa';
+import Path from 'path';
+import Bunyan from 'bunyan';
+
