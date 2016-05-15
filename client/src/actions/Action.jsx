72270d1a3aac0769dc3aabd48e643941fbdca8ac
Kpyu <hellomozilla@gmail.com>
2016-05-16 03:30:36 +0800

    添加登录action

--- client/src/actions/Action.jsx
+++ client/src/actions/Action.jsx
@@ -1,5 +1,39 @@
 import { LOGIN } from './ActionTypes.jsx';
+import request from 'axios';
+import { pushState } from 'redux-react-router';
+
+function doLogin() {
+  return { type: 'fetching' };
+}
+
+function loginSuccess(data) {
+  return { type: 'success', success: data };
+}
+
+function receiveError(error) {
+  return { type: 'falied', fail: error };
+}
+
 export function submit(data) {
   return LOGIN.SUBMIT_TODO;
 }
 
+
+
+export function login() {
+  return (dispatch) => {
+    dispatch(doLogin());
+    return request({
+      url: '/dologin',
+      timeout: 20000,
+      method: 'post',
+      responseType: 'json'
+    }).
+      then(function (response) {
+        dispatch(loginSuccess(response.data));
+      })
+      .catch(function (response) {
+        dispatch(receiveError(response.data));
+      });
+  };
+}
